ba0a34e5bf57fdd58a0a835b817fd33ef61a480d
Selvakumar-18 <selvakumarbalasubramani4@gmail.com>
2021-08-13 14:46:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const[data,setData] = useState(null);
+  const [loading,setLoading]=useState(true);
+  const [error,setError] =useState(null);
+
+  useEffect(()=>{
+    axios("https://reqres.in/api/users?page=2")
+    .then((response)=>{
+        setData(response.data);
+        console.log(response.data.data[3].first_name);
+    })
+    .catch((error)=>{
+      console.error("error fetch data:",error);
+      setError(error);
+    })
+    .finally(()=>{
+      setLoading(false);
+    });
+
+  },[]);
+
+  if (loading) return "loading";
+  if (error) return "error";
+
+  return(
+    <div className="table">
+      <h1 className="heading">User Data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>S.no</th>
+            <th>Id</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Image</th>
+            
+          </tr>
+        </thead>
+      
+      <tbody>
+        {
+          data.data.map((data,index)=>{
+            return(
+              <tr key={index}>
+                <td>{index+1}</td>
+                <td>{data.id}</td>
+                <td>{data.email}</td>
+                <td>{data.first_name}</td>
+                <td>{data.last_name}</td>
+                <td>
+                  <img src={data.avatar}/>
+                </td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+      </table>
+      {/* <pre>{JSON.stringify(data,null,2)}</pre> */}
     </div>
   );
 }
-
-export default App;
